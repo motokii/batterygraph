7e3cab8c87542cafbc9b48c718ba5c2bbcfd47c2
motoki nagaoka <motoki.23ar@gmail.com>
2016-12-10 15:18:20 +0900

    どこのディレクトリに置いても動くようにした

--- load.sh
+++ load.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-launchctl load plist/currentBattery.plist
-launchctl load plist/cycleBattery.plist
-launchctl load plist/maxBattery.plist
+
+CURRENT_DIR=`dirname $0`
+cd $CURRENT_DIR
+pwd
+echo ""
+./plist/currentBatteryCreater.sh
+./plist/cycleBatteryCreater.sh
+./plist/maxBatteryCreater.sh
+
+launchctl load $CURRENT_DIR/plist/currentBattery.plist
+launchctl load $CURRENT_DIR/plist/cycleBattery.plist
+launchctl load $CURRENT_DIR/plist/maxBattery.plist
 
--- unload.sh
+++ unload.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-launchctl unload plist/currentBattery.plist
-launchctl unload plist/cycleBattery.plist
-launchctl unload plist/maxBattery.plist
+
+CURRENT_DIR=`dirname $0`
+cd $CURRENT_DIR
+pwd
+echo ""
+
+launchctl unload $CURRENT_DIR/plist/currentBattery.plist
+launchctl unload $CURRENT_DIR/plist/cycleBattery.plist
+launchctl unload $CURRENT_DIR/plist/maxBattery.plist
 
