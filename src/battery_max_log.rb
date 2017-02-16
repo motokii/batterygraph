a3991e10123ab3f82caaa19351a543563988d102
motoki nagaoka <motoki.23ar@gmail.com>
2017-02-17 04:01:03 +0900

    ログの書き込みを作り変えたので削除

--- src/battery_max_log.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-require 'date'
-
-# 現在の日付を取得し、フォーマット変換して文字列出力
-now = Date.today.strftime('%Y/%m/%d')
-capacity = `/usr/sbin/ioreg -l | grep MaxCapacity`.chomp.split(" = ")[1]
-
-puts now+","+capacity
